9463ead2feec95993262b1b7e7b5889638d65438
LAKSHMANAPRAKASH S B <50242788+LatchuBright1402@users.noreply.github.com>
2022-07-16 20:51:13 +0530

    Add files via upload

---
+++ DifferentSet.py
@@ -0,0 +1,14 @@
+E = {0, 2, 4, 6, 8};
+N = {1, 2, 3, 4, 5};
+
+# union
+print("Union of E and N is",E | N)
+
+# intersection
+print("Intersection of E and N is",E & N)
+
+# difference
+print("Difference of E and N is",E - N)
+
+# symmetric difference
+print("Symmetric difference of E and N is",E ^ N)
